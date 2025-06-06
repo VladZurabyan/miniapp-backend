70d254225de38d656cce5aeba9ac055296ac34be
Vlad <vle-zur@yandex.ru>
2025-06-06 20:29:31 +0400

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Table, Column, Integer, String, Boolean, Float, DateTime, ForeignKey, Text
+from sqlalchemy import Table, Column, BigInteger, String, Boolean, Float, DateTime, ForeignKey, Text
 from sqlalchemy.sql import func
 from db import metadata
 
 users = Table(
     "users",
     metadata,
-    Column("id", Integer, primary_key=True, nullable=False),
+    Column("id", BigInteger, primary_key=True, nullable=False),  # ← исправлено
     Column("username", String(100), nullable=False),
     Column("ton_balance", Float, nullable=False, default=0.0),
     Column("usdt_balance", Float, nullable=False, default=0.0),
@@ -15,7 +15,7 @@ games = Table(
     "games",
     metadata,
     Column("id", String, primary_key=True, nullable=False),
-    Column("user_id", Integer, ForeignKey("users.id"), nullable=False),
+    Column("user_id", BigInteger, ForeignKey("users.id"), nullable=False),  # ← исправлено
     Column("game", String(50), nullable=False),
     Column("bet", Float, nullable=False),
     Column("result", Text, nullable=False),
