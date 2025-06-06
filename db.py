98d32e1bd9500ebbbd92861c1f04404b871dfbce
Vlad <vle-zur@yandex.ru>
2025-06-06 19:22:58 +0400

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from databases import Database
 from sqlalchemy import create_engine, MetaData
 
-DATABASE_URL = "postgresql://admin:...@dpg-d11gaf0gjchc73807k4g-a.oregon-postgres.render.com/miniapp"
+DATABASE_URL = "postgresql://admin:kfL519NsqWjfOgRe5eUQqZF3QFSgefNX@dpg-d11gaf0gjchc73807k4g-a.oregon-postgres.render.com/miniapp"
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
