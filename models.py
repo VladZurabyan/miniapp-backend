6173ac3e8769de79dc4c89ef254381366a18cf46
Vlad <vle-zur@yandex.ru>
2025-06-08 18:49:25 +0400

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Table, Column, BigInteger, String, Boolean, Float, DateTime, ForeignKey, Text
 from sqlalchemy.sql import func
 from db import metadata
+from sqlalchemy import Integer, JSON
 
 users = Table(
     "users",
@@ -21,4 +22,18 @@ games = Table(
     Column("result", Text, nullable=False),
     Column("win", Boolean, nullable=False),
     Column("timestamp", DateTime(timezone=True), server_default=func.now(), nullable=False),
+)
+
+safe_sessions = Table(
+    "safe_sessions",
+    metadata,
+    Column("id", String, primary_key=True, nullable=False),  # UUID или любой уникальный ID игры
+    Column("user_id", BigInteger, ForeignKey("users.id"), nullable=False),
+    Column("currency", String(10), nullable=False),          # 'ton' или 'usdt'
+    Column("bet", Float, nullable=False),
+    Column("code", JSON, nullable=False),                    # Например: [1, 4, 7]
+    Column("attempts", Integer, default=0, nullable=False),
+    Column("used_hint", Boolean, default=False, nullable=False),
+    Column("is_finished", Boolean, default=False, nullable=False),
+    Column("created_at", DateTime(timezone=True), server_default=func.now(), nullable=False),
 )
